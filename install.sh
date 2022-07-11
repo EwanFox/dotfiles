d439ef1e41e2725f52ed899973e28c009ccd0efe
Ewan James Fox <ewan@ewanfox.com>
2022-07-11 09:31:54 -0400

    Add zshrc support

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ SCRIPTR=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 dotfiledir=$(dirname "$SCRIPTR")
 # list of files/folders to symlink in ${homedir}
-files="bash_profile bashrc bash_prompt aliases private"
+files="bash_profile bashrc bash_prompt aliases private zshrc"
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
